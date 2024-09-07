f560d2591b17006d2f1a9d1b42626e7a69b286be
Nick M. <32173425+nickj609@users.noreply.github.com>
2024-09-06 23:35:05 -0400

    Update ICustomVoteApi.cs

--- CS2-CustomVotes.Shared/ICustomVoteApi.cs
+++ CS2-CustomVotes.Shared/ICustomVoteApi.cs
@@ -9,5 +9,6 @@ public interface ICustomVoteApi
 
     public void AddCustomVote(string name, string description, string defaultOption, float timeToVote, Dictionary<string, VoteOption> options, string style, int minVotePercentage);
     public void AddCustomVote(string name, List<string> aliases, string description, string defaultOption, float timeToVote, Dictionary<string, VoteOption> options, string style, int minVotePercentage);
+    public void StartCustomVote(CCSPlayerController? player, string name);
     public void RemoveCustomVote(string name);
 }
